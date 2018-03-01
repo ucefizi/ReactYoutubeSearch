bf0ffc525d9ae66188b4b5ad100bdc61f225bca1
Youssef IZIKITNE <youssefizikitn@gmail.com>
2018-03-01 23:18:27 +0000

    Commit message cannot be empty

--- README.md
+++ README.md
@@ -3,23 +3,11 @@
 Interested in learning [Redux](https://www.udemy.com/react-redux/)?
 
 ### Getting Started
-
-There are two methods for getting started with this repo.
-
-#### Familiar with Git?
-Checkout this repo, install dependencies, then start the gulp process with the following:
+Clone the repo, install dependencies and start the server. Then open http://localhost:3000/ on your browser.
 
 ```
-> git clone https://github.com/StephenGrider/ReduxSimpleStarter.git
-> cd ReduxSimpleStarter
-> npm install
-> npm start
-```
-
-#### Not Familiar with Git?
-Click [here](https://github.com/StephenGrider/ReactStarter/releases) then download the .zip file.  Extract the contents of the zip file, then open your terminal, change to the project directory, and:
-
-```
+> git clone https://github.com/youssefizikitn/ReactYoutubeSearch.git
+> cd ReactYoutubeSearch
 > npm install
 > npm start
 ```
--- src/index.js
+++ src/index.js
@@ -1,15 +1,53 @@
-import React from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'YOUR_YOUTUBE_API_KEY';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+//Create component
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+		this.videoSearch('never gonna give you up');
+
+	}
+
+	videoSearch(term) {
+		YTSearch({
+				key: API_KEY,
+				term: term
+			}, videos => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
+		});
+	}
+
+	render() {
+		const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 500);
+
+		return (
+			<div>
+				<SearchBar onSearchTermChange={videoSearch} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList
+					videos={this.state.videos}
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+				/>
+			</div>
+		);
+	}
+}
+
+//Render HTML
+ReactDOM.render(<App />, document.querySelector('.container'));
